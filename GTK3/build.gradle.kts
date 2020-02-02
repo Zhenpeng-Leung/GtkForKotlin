2a2a1eb132b7ec944ddd3ae4d2034cd627e095a6
Zhenpeng Leung <liangzp1990520@outlook.com>
2020-02-02 22:57:37 +0800

    Fixed Windows platform not compiling

--- GTK3/build.gradle.kts
+++ GTK3/build.gradle.kts
@@ -12,7 +12,43 @@ kotlin {
         else -> throw Throwable("Unsupported system host: ${HostManager.hostName}")
     }
     native.apply {
-        compilations["main"].cinterops.create("gtk3")
+        compilations["main"].cinterops.create("gtk3") {
+            when (true) {
+                HostManager.hostIsMac, HostManager.hostIsLinux -> {
+                    listOf("/opt/local/include", "/usr/include", "/usr/local/include").forEach {
+                        includeDirs(
+                            "$it/atk-1.0",
+                            "$it/gdk-pixbuf-2.0",
+                            "$it/cairo",
+                            "$it/harfbuzz",
+                            "$it/pango-1.0",
+                            "$it/gtk-3.0",
+                            "$it/glib-2.0"
+                        )
+                    }
+
+                    includeDirs(
+                        "/opt/local/lib/glib-2.0/include",
+                        "/usr/lib/x86_64-linux-gnu/glib-2.0/include",
+                        "/usr/local/lib/glib-2.0/include"
+                    )
+                }
+                HostManager.hostIsMingw -> {
+                    val mingwPath= file(System.getenv("MINGW64_DIR") ?: "C:/msys64/mingw64")
+                    listOf(
+                        "include/atk-1.0",
+                        "include/gdk-pixbuf-2.0",
+                        "include/cairo",
+                        "include/pango-1.0",
+                        "include/gtk-3.0",
+                        "include/glib-2.0",
+                        "lib/glib-2.0/include"
+                    ).forEach {
+                        includeDirs(mingwPath.resolve(it))
+                    }
+                }
+            }
+        }
         binaries.staticLib(listOf(RELEASE))
     }
 }
